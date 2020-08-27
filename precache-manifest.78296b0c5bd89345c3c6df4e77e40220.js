2a45ff257fe8b48e6543dff109210712a43a5e99
Alexey <alexey6486@gmail.com>
2020-08-27 11:28:04 +0300

    Updates

---
+++ precache-manifest.78296b0c5bd89345c3c6df4e77e40220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ec4f4cda7a2c3cda9a0dd9d90fffad",
+    "url": "/itk_youtube_ts/index.html"
+  },
+  {
+    "revision": "2065eff5a1b08ae744ff",
+    "url": "/itk_youtube_ts/static/css/main.5cd32847.chunk.css"
+  },
+  {
+    "revision": "7eb70db0908874a9575b",
+    "url": "/itk_youtube_ts/static/js/2.41606121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/itk_youtube_ts/static/js/2.41606121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2065eff5a1b08ae744ff",
+    "url": "/itk_youtube_ts/static/js/main.d297539c.chunk.js"
+  },
+  {
+    "revision": "a202341d83bc34c90dbb",
+    "url": "/itk_youtube_ts/static/js/runtime-main.0b07e6bd.js"
+  },
+  {
+    "revision": "a09689b48627ac3c2c924baeecd4ecaf",
+    "url": "/itk_youtube_ts/static/media/loading.a09689b4.svg"
+  }
+]);
