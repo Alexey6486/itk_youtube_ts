9154555f4c9580e1b2b195e196e38ac64c43f61b
Alexey <alexey6486@gmail.com>
2020-08-27 11:33:39 +0300

    changes_27-08(01)

--- src/components/common/loadingIcon/LoadingIcon.tsx
+++ src/components/common/loadingIcon/LoadingIcon.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import loadingIcon from "../../../assets/images/loading.svg";
+import s from './LoadingIcon.module.css';
 
 export const LoadingIcon = () => {
 
     return (
-        <div>
+        <div className={s.loadingIcon}>
             <img src={loadingIcon} alt={'loading icon'}/>
         </div>
     );
